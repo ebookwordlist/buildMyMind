d189e7c3ac35cb8a9c7553c9f287f67f3eb662e3
pencilCool <yhtangcoder@gmail.com>
2022-03-13 22:38:04 +0800

    update

--- source/to.py
+++ source/to.py
@@ -7,6 +7,8 @@ a = list(t)
 
 for k in a:
     print(k)
-    commnd = "cat {}/{}.txt | xargs -I{{}} swift ./test.swift {{}} > ./{}/mac_{}.md".format(
-        k, k, k, k, k)
+    # commnd = "cat {}/{}.txt | xargs -I{{}} swift ./test.swift {{}} > ./{}/mac_{}.md".format(
+    # k, k, k, k, k)
+
+    commnd = "mv ./{}/mac_{}.md ./{}/mac_{}.txt".format(k, k, k, k)
     os.system(commnd)
